e117f9ea606bbb03c70c4f882168d88b7fabc9ca
pm6953 <pm.schratt@mci4me.at>
2025-03-26 11:13:38 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+from my_functions import build_person
+if __name__ == "__main__":
+    print(build_person("Pia", "Schratt","female", 21))
+
+from my_functions import build_experiment
+if __name__ == "__main__":
+    print(build_experiment("estimate_max_hr()", "2025-03-24", "Julian Huber","Aufgabe 3"))
