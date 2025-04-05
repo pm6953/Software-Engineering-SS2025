8a249ba0922ed88778fabbd3bdcabae492072a5d
pm6953 <pm.schratt@mci4me.at>
2025-04-05 17:51:03 +0200

    Aufgabe 6.2

--- my_classes.py
+++ my_classes.py
@@ -1,24 +1,38 @@
-class Subject():
-    def __init__(self, first_name, last_name, age, sex):
+from datetime import datetime
+
+class Person():
+    def __init__(self, first_name, last_name, date_of_birth, sex):
         self.first_name = first_name
         self.last_name = last_name
-        self.age = age
-        self.sex = sex
+        self.__date_of_birth = date_of_birth #__ vor date zum Verstecken
+        self.sex = sex 
+    
+    def get_age(self):
+        date_format = "%Y-%m-%d %I:%M%p" #Format für die Eingabe des Geburtsdatums
+        date_of_birth_temp = datetime.strptime(self.__date_of_birth, date_format)
+        age = datetime.today().year - date_of_birth_temp.year
+        return age
+    
+        
+    
+class Subject(Person):
+    def __init__(self, first_name, last_name, date_of_birth, sex):
+        super().__init__(first_name, last_name, date_of_birth, sex)
+        
 
     def estimate_max_hr(self) -> int: 
         if self.sex == "male":
-            max_hr_bpm =  223 - 0.9 * self.age
+            max_hr_bpm =  223 - 0.9 * super().get_age()
         elif self.sex == "female":
-            max_hr_bpm = 226 - 1.0 *  self.age
+            max_hr_bpm = 226 - 1.0 *  super().get_age()
         else:
             max_hr_bpm  = 0 #Meldung bei Fehler
         return int(max_hr_bpm)
         
 
-class Supervisor():
+class Supervisor(Person):
     def __init__(self, first_name, last_name):
-        self.first_name = first_name
-        self.last_name = last_name
+        super().__init__(first_name, last_name, date_of_birth = None, sex= None)
        
 class Experiment():
     def __init__(self, name, date):
--- test.py
+++ test.py
@@ -2,14 +2,14 @@ from my_classes import Subject, Supervisor, Experiment
 
 if __name__ == "__main__":
 
-    # Erstellen einer Fahrradergometer-Leistungstest
+    # Erstellen eines Leistungstest
     supervisor = Supervisor("Pia", "Schratt")
-    subject = Subject("first_name", "last_name", "male", 24)
+    subject = Subject("first_name", "last_name","2024-12-04 10:07AM", "sex")
     subject.estimate_max_hr()
 
-    experiment = Experiment("Fahrradergometer-Leistungstest", "2025-04-05")
+    experiment = Experiment("Leistungstest", "2025-04-05")
     experiment.add_subject(subject)
     experiment.add_supervisor(supervisor)
 
 
-    print(experiment)
+    print("experiment")
